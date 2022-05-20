aa3999e1e9b88f8b3037987297fd3a5508e0a4ab
Makuza Mugabo Verite <mugaboverite@gmail.com>
2022-05-20 13:04:39 +0200

    feat: setup tests

---
+++ playwright.config.ts
@@ -0,0 +1,58 @@
+import { PlaywrightTestConfig, devices } from '@playwright/test'
+import path from 'path'
+
+// Reference: https://playwright.dev/docs/test-configuration
+const config: PlaywrightTestConfig = {
+  timeout: 30 * 1000,
+  testDir: path.join(__dirname, 'e2e'),
+  retries: 2,
+  outputDir: 'test-results/',
+
+  // Run your local dev server before starting the tests:
+  // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
+  webServer: {
+    command: 'npm run dev',
+    port: 3000,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
+
+  use: {
+
+    trace: 'retry-with-trace',
+
+  },
+
+  projects: [
+    {
+      name: 'Desktop Chrome',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+    // {
+    //   name: 'Desktop Firefox',
+    //   use: {
+    //     ...devices['Desktop Firefox'],
+    //   },
+    // },
+    // {
+    //   name: 'Desktop Safari',
+    //   use: {
+    //     ...devices['Desktop Safari'],
+    //   },
+    // },
+    // Test against mobile viewports.
+    {
+      name: 'Mobile Chrome',
+      use: {
+        ...devices['Pixel 5'],
+      },
+    },
+    {
+      name: 'Mobile Safari',
+      use: devices['iPhone 12'],
+    },
+  ],
+}
+export default config
