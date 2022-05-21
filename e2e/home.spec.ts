6a498ae473b1bdd2e1746e5d7f9d7dbd8397fa87
EGOIST <hi@egoist.sh>
2022-05-21 16:50:29 +0800

    only test chromium for now

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -1,8 +1,6 @@
+import { expect, test } from "@playwright/test"
 
-   
-import { expect, test } from '@playwright/test';
-
-test('Home page should load', async ({ page }) => {
-	await page.goto('/');
-	expect(await page.textContent('h1')).toBe("Proselog");
-});
+test("Home page should load", async ({ page }) => {
+  await page.goto("/")
+  expect(await page.textContent("h1")).toBe("Proselog")
+})
