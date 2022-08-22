e25698746e60ec6c7a29f70c497f836047d34294
James Berry <jb@jamesbe.com>
2022-08-22 19:30:06 +0100

    added nextjs-cors

--- src/pages/api/v0/[...trpc].ts
+++ src/pages/api/v0/[...trpc].ts
@@ -1,8 +1,18 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import cors from 'nextjs-cors';
 import { createOpenApiNextHandler } from "trpc-openapi"
 import { getTRPCContext } from "~/lib/trpc.server"
 import { appRouter } from "~/router"
 
-export default createOpenApiNextHandler({
-  router: appRouter,
-  createContext: getTRPCContext,
-})
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  // Setup CORS
+  await cors(req, res);
+  
+  // Handle incoming OpenAPI requests
+  return createOpenApiNextHandler({
+    router: appRouter,
+    createContext: getTRPCContext,
+  })(req, res);
+};
+
+export default handler;
